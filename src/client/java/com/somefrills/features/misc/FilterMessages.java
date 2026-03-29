c5e3feda9ce98d6d25b009d1a8c43bb955667968
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-03-29 21:47:42 +0300

    fix FilterMessages

--- src/client/java/com/somefrills/features/misc/FilterMessages.java
+++ src/client/java/com/somefrills/features/misc/FilterMessages.java
@@ -1,5 +1,6 @@
 package com.somefrills.features.misc;
 import com.somefrills.config.Feature;
+import com.somefrills.events.ChatMsgEvent;
 import com.somefrills.events.OverlayMsgEvent;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.util.Formatting;
@@ -14,18 +15,16 @@ public class FilterMessages {
             Pattern.compile("Your Implosion hit (\\d+) enem(?:y|ies) for ([\\d,.]+) damage\\.");
 
     @EventHandler
-    private static void onChatMessage(OverlayMsgEvent event) {
-        String msg = Formatting.strip(event.getMessage().getString().strip());
-        if (shouldFilter(msg)) {
+    private static void onChatMessage(ChatMsgEvent event) {
+        if (shouldFilter(event.messagePlain)) {
             event.cancel();
         }
     }
 
     private static boolean shouldFilter(String msg) {
-        if (msg.equals("There are blocks in the way!")) {
+        if (msg.contains("There are blocks in the way!")) {
             return true;
         }
-
         return IMPLOSION_PATTERN.matcher(msg).matches();
     }
 }
