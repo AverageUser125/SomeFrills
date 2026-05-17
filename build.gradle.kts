b53071aeea60e5aec73895072a6052e85cac8641
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-05-17 22:57:37 +0300

    add skyblock_enchancments as a compile dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("fabric-loom") version "1.15.5"
+    id("fabric-loom") version "1.16.2"
     id("maven-publish")
     id("org.jetbrains.kotlin.jvm") version "2.3.0"
     id("com.google.devtools.ksp") version "2.3.8"
@@ -57,22 +57,27 @@ dependencies {
     mappings("net.fabricmc:yarn:${project.extra["yarn_mappings"]}:v2")
     modImplementation("net.fabricmc:fabric-loader:${project.extra["loader_version"]}")
 
-    // Fabric API. This is technically optional, but you probably want it anyway.
-    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.extra["fabric_version"]}")
-
+    // Internal dependencies
     modImplementation("meteordevelopment:orbit:${project.extra["orbit_version"]}")
     include("meteordevelopment:orbit:${project.extra["orbit_version"]}")
-    // MoulConfig (artifact uses mapping suffix modern-1.21.11)
     modImplementation("org.notenoughupdates.moulconfig:modern-1.21.11:${project.extra["moulconfig_version"]}")
     include("org.notenoughupdates.moulconfig:modern-1.21.11:${project.extra["moulconfig_version"]}")
-    modImplementation("net.fabricmc:fabric-language-kotlin:1.13.11+kotlin.2.3.21")
-
-    compileOnly(files("vendor/SkyHanni-7.19.0-mc1.21.11.jar"))
     modImplementation("moe.nea:libautoupdate:1.3.1")
     include("moe.nea:libautoupdate:1.3.1")
 
-    modImplementation("com.terraformersmc:modmenu:${project.extra["modmenu_version"]}")
+
+    // External mods we optionally modify
+    compileOnly(files("vendor/SkyHanni-7.19.0-mc1.21.11.jar"))
     modCompileOnly(files("vendor/SkyHanni-7.19.0-mc1.21.11.jar"))
+
+    compileOnly(files("vendor/skyblock_enhancements-1.0.1+1.21.11.jar"))
+    modCompileOnly(files("vendor/skyblock_enhancements-1.0.1+1.21.11.jar"))
+
+
+    // External dependencies
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.extra["fabric_version"]}")
+    modImplementation("com.terraformersmc:modmenu:${project.extra["modmenu_version"]}")
+    modImplementation("net.fabricmc:fabric-language-kotlin:1.13.11+kotlin.2.3.21")
 
     ksp(project(":processor"))
 }
