a9b15af304b73ae8e8682962e6b9ae464035527f
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2026-05-18 14:48:59 +0300

    fix null posibile event

--- src/main/kotlin/com/somefrills/events/SlotClickEvent.kt
+++ src/main/kotlin/com/somefrills/events/SlotClickEvent.kt
@@ -5,7 +5,7 @@ import net.minecraft.screen.slot.Slot
 import net.minecraft.screen.slot.SlotActionType
 
 class SlotClickEvent(
-    val slot: Slot,
+    val slot: Slot?,
     val slotId: Int,
     val button: Int,
     val actionType: SlotActionType,
